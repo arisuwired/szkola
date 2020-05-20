c7a3207f8986cfbf8232df3ad1127f1772827765
alice <arisuwired@gmail.com>
2020-05-20 13:34:50 +0200

    aa

--- wieza_hanoi/hanoi.py
+++ wieza_hanoi/hanoi.py
@@ -101,6 +101,8 @@ rods = [Rod(
         ) for i in range(3)]
 ilosc_dyskow = 4
 min_ruchy = 0
+binary_clock = 0
+print_clock = False
 ruchy = 0
 to_move = []
 win = False
@@ -113,11 +115,14 @@ def init_game(n):
     global min_ruchy
     global ruchy
     global last_rod
+    global binary_clock
+
     ilosc_dyskow = n
     min_ruchy = 2**ilosc_dyskow - 1
     ruchy = 0
     last_rod = None
     win = False
+    binary_clock = 0
     for rod in rods:
         rod.disks = []
     rods[0].disks = create_disks(ilosc_dyskow)
@@ -129,14 +134,27 @@ def menu():
     root.geometry('350x200')
     chk_godmode = tkinter.BooleanVar()
     chk_godmode.set(False)
+    chk_clock = tkinter.BooleanVar()
+    chk_clock.set(False)
 
     def change_godmode():
         global godmode
         godmode = chk_godmode.get()
 
+    def change_clock():
+        global print_clock
+        print_clock = chk_clock.get()
+
+    def reset_clock():
+        global binary_clock
+        binary_clock = 0
+
     chk = tkinter.Checkbutton(root, text='GODMODE', var=chk_godmode,
             command=change_godmode)
     chk.grid(column=0, row=0)
+    chk2 = tkinter.Checkbutton(root, text='binary clock', var=chk_clock,
+            command=change_clock)
+    chk2.grid(column=1, row=0)
 
     lbl = tkinter.Label(root, text="dyski: ")
     lbl.grid(column=0, row=1)
@@ -148,6 +166,8 @@ def menu():
 
     btn = tkinter.Button(root, text="RESET", command=clicked)
     btn.grid(column=2, row=1)
+    btn2 = tkinter.Button(root, text="reset clock", command=reset_clock)
+    btn2.grid(column=2, row=0)
 
     root.mainloop()
     
@@ -159,7 +179,7 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        if win:
+        elif win:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 init_game(ilosc_dyskow)
             continue
@@ -194,6 +214,10 @@ while True:
                     rod = last_rod
                 while to_move:
                     rod.disks.append(to_move.pop())
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_c:
+                binary_clock += 1
+
     screen.fill(bg_color)
 
     for rod in rods:
@@ -216,6 +240,13 @@ while True:
     text = 'aktualna ilosc ruchow: ' + str(ruchy)
     ren = font.render(text, 0, (255, 255, 255), screen)
     screen.blit(ren, (10, 50))
+
+    if print_clock:
+        font = pygame.font.Font(None, 100)
+        number_format = f"{{:#0{2+ilosc_dyskow}b}}"
+        ren = font.render(number_format.format(binary_clock),
+                0, (255, 255, 255), screen)
+        screen.blit(ren, (10, 100))
 
     if win:
         font = pygame.font.Font(None, 200)
